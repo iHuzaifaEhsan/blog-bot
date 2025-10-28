9ea165c6377b9d1de38fe1485a1d88b157ace207
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-10-28 20:26:55 +0500

    Fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             ))}
         </div>
       </div>
-      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 &copy; BlogBot - AI Powered blogging site | All Rights Reserved.</p>
+      <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 &copy; BlogBot - AI Powered blogging site | All Rights Reserved </p>
     </div>
   )
 }
